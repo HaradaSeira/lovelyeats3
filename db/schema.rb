d0afc7becd02007adaf7debc7b8e3d6348af4ebf
EC2 Default User <ec2-user@ip-172-31-13-4.ap-southeast-2.compute.internal>
2023-08-25 02:27:40 +0000

    Shopモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_073344) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_022334) do
   create_table "locations", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "shops", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
@@ -30,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_073344) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shops", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.first
+
+Shop.create!(
+  name: "Sample Shop 1",
+  description: "This is a sample shop 1.",
+  user: user
+)
+
+Shop.create!(
+  name: "Sample Shop 2",
+  description: "This is a sample shop 2.",
+  user: user
+)

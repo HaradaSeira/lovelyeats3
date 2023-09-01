7cb8830bb3012411a00b1359212a1f4c7d001d7d
EC2 Default User <ec2-user@ip-172-31-13-4.ap-southeast-2.compute.internal>
2023-09-01 07:52:07 +0000

    eventの日付形式がうまく行かない

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,25 +5,46 @@ class EventsController < ApplicationController
   end
   
   def create
+    Rails.logger.debug(params.inspect) 
     @event = Event.new(event_params)
-    if @event.save
+    
+    if @event.valid?
+      # ここでデバッグ出力
+      puts "opened_atの値: #{@event.opened_at}"
+      @event.save # データベースに保存
       redirect_to user_dashboard_path, notice: 'イベントを登録しました。'
     else
       flash.now[:alert] = @event.errors.full_messages.join(', ')
       render :new
     end
   end
-  
+
   private
   
-  def event_params
-    params.require(:event).permit(
-    :event_date,
-    :start_time,
-    :end_time,
-    :location_id,
-    :address,
-    :shop_id
-  )
-  end
+      def event_params
+        params.require(:event).permit(
+          :location_id,
+          :address,
+          :shop_id,
+          :opened_at_date,
+          :opened_at_time,
+          :closed_at_date,
+          :closed_at_time
+        ).tap do |whitelisted|
+          whitelisted[:opened_at] = DateTime.new(
+            whitelisted["opened_at_date(1i)"].to_i,
+            whitelisted["opened_at_date(2i)"].to_i,
+            whitelisted["opened_at_date(3i)"].to_i,
+            whitelisted["opened_at_time(4i)"].to_i,
+            whitelisted["opened_at_time(5i)"].to_i
+          )
+          whitelisted[:closed_at] = DateTime.new(
+            whitelisted["closed_at_date(1i)"].to_i,
+            whitelisted["closed_at_date(2i)"].to_i,
+            whitelisted["closed_at_date(3i)"].to_i,
+            whitelisted["closed_at_time(4i)"].to_i,
+            whitelisted["closed_at_time(5i)"].to_i
+          )
+        end
+      end
 end

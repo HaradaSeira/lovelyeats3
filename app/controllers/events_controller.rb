83d58a5804669501b3893f38a0daedf3ce72b433
EC2 Default User <ec2-user@ip-172-31-13-4.ap-southeast-2.compute.internal>
2023-09-04 09:44:19 +0000

    イベントshow

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,18 @@
 class EventsController < ApplicationController
+  def show
+    @shop = Shop.find(params[:shop_id])
+    @event = @shop.events.find(params[:id])
+  end
+
   def new
     @event = Event.new
-    @locations = Location.all 
+    @locations = Location.all
   end
-  
+
   def create
-    Rails.logger.debug(params.inspect) 
+    Rails.logger.debug(params.inspect)
     @event = Event.new(event_params)
-    
+
     if @event.valid?
       # ここでデバッグ出力
       puts "opened_atの値: #{@event.opened_at}"
@@ -20,31 +25,31 @@ class EventsController < ApplicationController
   end
 
   private
-  
-      def event_params
-        params.require(:event).permit(
-          :location_id,
-          :address,
-          :shop_id,
-          :opened_at_date,
-          :opened_at_time,
-          :closed_at_date,
-          :closed_at_time
-        ).tap do |whitelisted|
-          whitelisted[:opened_at] = DateTime.new(
-            whitelisted["opened_at_date(1i)"].to_i,
-            whitelisted["opened_at_date(2i)"].to_i,
-            whitelisted["opened_at_date(3i)"].to_i,
-            whitelisted["opened_at_time(4i)"].to_i,
-            whitelisted["opened_at_time(5i)"].to_i
-          )
-          whitelisted[:closed_at] = DateTime.new(
-            whitelisted["closed_at_date(1i)"].to_i,
-            whitelisted["closed_at_date(2i)"].to_i,
-            whitelisted["closed_at_date(3i)"].to_i,
-            whitelisted["closed_at_time(4i)"].to_i,
-            whitelisted["closed_at_time(5i)"].to_i
-          )
-        end
-      end
+
+  def event_params
+    params.require(:event).permit(
+      :location_id,
+      :address,
+      :shop_id,
+      :opened_at_date,
+      :opened_at_time,
+      :closed_at_date,
+      :closed_at_time
+    ).tap do |whitelisted|
+      whitelisted[:opened_at] = DateTime.new(
+        whitelisted["opened_at_date(1i)"].to_i,
+        whitelisted["opened_at_date(2i)"].to_i,
+        whitelisted["opened_at_date(3i)"].to_i,
+        whitelisted["opened_at_time(4i)"].to_i,
+        whitelisted["opened_at_time(5i)"].to_i
+      )
+      whitelisted[:closed_at] = DateTime.new(
+        whitelisted["closed_at_date(1i)"].to_i,
+        whitelisted["closed_at_date(2i)"].to_i,
+        whitelisted["closed_at_date(3i)"].to_i,
+        whitelisted["closed_at_time(4i)"].to_i,
+        whitelisted["closed_at_time(5i)"].to_i
+      )
+    end
+  end
 end

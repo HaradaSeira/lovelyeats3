2618c7ca32f0903292131f35c0ce76882cda4b02
HaradaSeira <starsomes19870920@gmail.com>
2023-09-05 09:35:20 +0000

    event controllerうまくいかない

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,30 +26,17 @@ class EventsController < ApplicationController
 
   private
 
-  def event_params
+   def event_params
     params.require(:event).permit(
       :location_id,
       :address,
       :shop_id,
-      :opened_at_date,
-      :opened_at_time,
-      :closed_at_date,
-      :closed_at_time
+      :event_date,
+      :start_time,
+      :end_time
     ).tap do |whitelisted|
-      whitelisted[:opened_at] = DateTime.new(
-        whitelisted["opened_at_date(1i)"].to_i,
-        whitelisted["opened_at_date(2i)"].to_i,
-        whitelisted["opened_at_date(3i)"].to_i,
-        whitelisted["opened_at_time(4i)"].to_i,
-        whitelisted["opened_at_time(5i)"].to_i
-      )
-      whitelisted[:closed_at] = DateTime.new(
-        whitelisted["closed_at_date(1i)"].to_i,
-        whitelisted["closed_at_date(2i)"].to_i,
-        whitelisted["closed_at_date(3i)"].to_i,
-        whitelisted["closed_at_time(4i)"].to_i,
-        whitelisted["closed_at_time(5i)"].to_i
-      )
+      whitelisted[:opened_at] = DateTime.parse("#{whitelisted[:event_date]} #{whitelisted[:start_time]}")
+      whitelisted[:closed_at] = DateTime.parse("#{whitelisted[:event_date]} #{whitelisted[:end_time]}")
     end
   end
 end

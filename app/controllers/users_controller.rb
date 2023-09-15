dfa6023bba2920fc936392b996e1636a60bfc193
HaradaSeira <starsomes19870920@gmail.com>
2023-09-15 09:54:32 +0000

    ログイン周りのエラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def dashboard
-    # @locations 変数にデータをセットするクエリを実行する
-    @locations = Location.all # 例: Location モデルからすべての場所を取得する
-    @shops = Shop.where(user_id: current_user.id) # ユーザーに関連するショップを取得する
-    @items = Item.where(user_id: current_user.id) # ユーザーに関連するメニューを取得する
-    
-    # 他のコード
+    if current_user
+      # @locations 変数にデータをセットするクエリを実行する
+      @locations = Location.all # 例: Location モデルからすべての場所を取得する
+      @shops = Shop.where(user_id: current_user.id) # ユーザーに関連するショップを取得する
+      @items = Item.where(user_id: current_user.id) # ユーザーに関連するメニューを取得する
+    else
+      redirect_to user_session_path, alert: 'ログインしてください'
+    end
   end
 end
